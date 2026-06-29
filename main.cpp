f0b979217939bf6b4a75244b460cc4088297aa65
qhatahet <qhatahet@gmail.com>
2026-06-29 17:30:39 +0300

    added messi

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main()
 {
     cout << "Hello, World!" << endl;
-    cout << "This is a simple C++ program." << endl;
+    cout << "This is a simple C++ program. messi" << endl;
     return 0;
 }
