fb0c3f90e8f22ef35c37dbed54cc136aafce7068
qhatahet <qhatahet@gmail.com>
2026-06-29 17:28:52 +0300

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main()
 {
     cout << "Hello, World!" << endl;
-    cout << "This is a simple C++ program." << endl;
+    cout << "This is a simple C++ program. qais hatahet" << endl;
     return 0;
 }
